7b17bd12f594a70af6c3f0027c6636152da6d800
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-03-02 10:13:40 -0500

    Update color scale

--- src/helpers.js
+++ src/helpers.js
@@ -4,15 +4,16 @@ import { format } from 'd3-format';
 export const colorize = (value, domain) => {
   return chroma
     .scale([
-      '#c7e9b4',
-      '#7fcdbb',
-      '#41b6c4',
-      '#1d91c0',
-      '#225ea8',
-      '#253494',
-      '#421496',
-      '#330963',
-      '#210433',
+      '#5e4fa2',
+      '#3288bd',
+      '#66c2a5',
+      '#abdda4',
+      '#e6f598',
+      '#fee08b',
+      '#fdae61',
+      '#f46d43',
+      '#d53e4f',
+      '#9e0142',
     ])
     .domain(domain)(value);
 };
